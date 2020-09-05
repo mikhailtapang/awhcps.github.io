bf0707dd769878ed62197c47c9b698d66b9849c1
mikhailtapang <mlatapang@gmail.com>
2020-09-05 13:45:49 +0800

    Initial commit

---
+++ include/banner.php
@@ -0,0 +1,12 @@
+<div class="container"> 
+<nav class="navbar navbar-inverse" style="margin-top:5px;">
+    <div class="container-fluid">
+     <div class="banner">
+                    <img src="../img/logo.jpg" width="200" height="90" style="position:absolute;margin-top:-20px;">
+<div style="color:White;margin-left:250px;margin-bottom:50px;margin-top:20px;font-size:30px;font-family:Bodoni MT Black;">AWH Vehicle Inventory and Ticketing System</div>
+
+               </div> 
+     
+
+     </div>
+    </nav>
